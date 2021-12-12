2ec97ce7947db195cde513bd407c2f04f00863ea
= <pandit.rohit0007@gmail.com>
2021-12-13 02:14:00 +0530

    codeforces round

---
+++ Life_of_a_Flower.cpp
@@ -0,0 +1,63 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define endl "\n"
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvl;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+#ifdef D_DEBUG
+  freopen("input.txt", "r", stdin)
+#endif
+      int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    vector<int> input(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> input[i];
+    }
+    int answer = (input[0] == 1 ? 1 : 0) + 1;
+    for (int i = 1; i < n; ++i) {
+      if (input[i] == 1 and input[i - 1] == 1) {
+        answer += 5;
+      } else if (input[i] == 1) {
+        answer += 1;
+      } else if (input[i] == 0 and input[i - 1] == 0) {
+        answer = -1;
+        break;
+      }
+    }
+    cout << answer << endl;
+  }
+}
