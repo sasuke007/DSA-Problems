7c65d9b6b3d659c4df8f4432f109e3b52038a630
= <pandit.rohit0007@gmail.com>
2022-06-27 05:56:28 +0530

    solved codeforces 1000 problem.

--- daily problems atomic habits/Circle_game.cpp
+++ daily problems atomic habits/Circle_game.cpp
@@ -1,5 +1,41 @@
-//
-// Created by Rohit Pandit on 27/06/22.
-//
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include<vector>
+#include<numeric>
+#include <algorithm>
+#include<list>
+#include<queue>
+#include <unordered_set>
+#include <cmath>
+#include<set>
 
-#include "Circle_game.h"
+#define endl "\n"
+using namespace std;
+
+int main() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n;
+        cin>>n;
+        vector<int> input(n);
+        for(int i=0;i<n;++i){
+            cin>>input[i];
+        }
+        if(n%2!=0){
+            cout<<"Mike"<<endl;
+        }
+        else{
+            int minimum = *min_element(input.begin(),input.end());
+            int index = find(input.begin(),input.end(),minimum) - input.begin();
+            if(index%2==0){
+                cout<<"Joe"<<endl;
+            }
+            else{
+                cout<<"Mike"<<endl;
+            }
+        }
+    }
+}
+
